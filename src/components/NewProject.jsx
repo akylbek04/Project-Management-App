255b46e269892289ec2ddd13faa91df6fca98698
Akylbek Zamirov <124916251+akylbek04@users.noreply.github.com>
2023-12-06 12:46:01 +0100

    Modal (#1)
    * created indepedent modal component using refs&imperativeHandle and portals
    
    * added referece to dialog tag inside Modal.jsx which was causing errors

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-const NewProject = ({ onAdd,  }) => {
+const NewProject = ({ onAdd }) => {
+  const modal = useRef();
+
   const title = useRef();
   const desc = useRef();
   const dueDate = useRef();
@@ -11,38 +14,48 @@ const NewProject = ({ onAdd,  }) => {
     const description = desc.current.value;
     const date = dueDate.current.value;
 
+    if (name.trim() === "" || description.trim() === "" || date.trim() === "") {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       name: name,
       description: description,
       date: date,
     });
-
-    
   };
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className="px-6 py-2 bg-stone-800 hover:bg-stone-950 text-stone-50 rounded-md"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" title="Title" isInput ref={title} />
-        <Input title="Description" ref={desc} />
-        <Input type="date" title="Due date" isInput ref={dueDate} />
+    <>
+      <Modal ref={modal}>
+        <h2></h2>
+        <p></p>
+        <p></p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 bg-stone-800 hover:bg-stone-950 text-stone-50 rounded-md"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" title="Title" isInput ref={title} />
+          <Input title="Description" ref={desc} />
+          <Input type="date" title="Due date" isInput ref={dueDate} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
